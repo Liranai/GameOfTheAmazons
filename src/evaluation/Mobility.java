53c3693739bfdb830caf7edbb3a9e1209ea6d3fa
MinimAI <felix.taschbach@outlook.com>
2014-11-19 15:14:55 +0100

    added territory eval

---
+++ src/evaluation/Mobility.java
@@ -0,0 +1,49 @@
+package evaluation;
+
+import java.awt.Point;
+import java.util.Vector;
+
+import evaluation.Evaluation;
+import model.Board;
+import model.Queen;
+
+/*
+ * Mobility = sum of the possible moves of all his/her queens
+ * Mobility feature =  subtracting the opponent’s mobility from the 
+player’s
+ */
+public class Mobility {
+	
+	/*
+	 * @return Mobility feature
+	 */
+	public static int getMobilityFeature(boolean colour, Board board){
+		int whiteMobility = getMobility(true,board);
+		int blackMobility = getMobility(false,board);
+		if(colour = true){
+			return whiteMobility - blackMobility;
+		}
+		else{
+			return blackMobility - whiteMobility;
+		}
+	}
+	
+	/*
+	 * sums up the possible moves of all queens of one colour
+	 */
+	public static int getMobility(boolean colour, Board board){
+		int mobility = 0;
+		Vector<Queen> queens = Evaluation.getQueens(colour, board);
+		for(Queen q : queens){
+			Point p = q.getPosition();
+			Vector<Point> moveLocations = Evaluation.getLocations(p,board);
+			for(Point o : moveLocations){
+				Vector<Point> arrowLocations = Evaluation.getLocations(o,board);
+				mobility += arrowLocations.size();
+			}
+		}
+		System.out.println("mob" + mobility);
+		return mobility;
+	}
+	
+}
