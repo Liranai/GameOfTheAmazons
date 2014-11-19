0d75e8017b90cc14b82225fc436a96fa8d3189ec
MinimAI <felix.taschbach@outlook.com>
2014-11-19 15:24:47 +0100

    Update Evaluation.java

--- src/evaluation/Evaluation.java
+++ src/evaluation/Evaluation.java
@@ -10,6 +10,105 @@ public class Evaluation {
 	private Board board;
 	private Vector<Queen> queens = new Vector<Queen>();
 
+	/*
+	 * fills the queens vector with the locations of all queens of one colour
+	 */
+	public static Vector<Queen> getQueens(boolean colour, Board board) {
+		Vector<Queen> tempqueens = board.getQueens();
+		Vector<Queen> queens = new Vector<Queen>();
+		for (int i = 0; i < tempqueens.size(); i++) {
+			int cntr = 0;
+			if (tempqueens.get(i).isColor() == colour) {
+				Queen queenToPlace = tempqueens.get(i);
+				queens.add(cntr, queenToPlace);
+				cntr++;
+			}
+		}
+		return queens;
+	}
+	
+	/*
+	 * Milan's code to find all Locations modified to return locations
+	 */
+	public static Vector<Point> getLocations(Point p, Board board){
+		Vector<Point> locations = new Vector<Point>();
+		boolean filled = false;
+		while (!filled) {
+			Point nextPoint = new Point(0, 0);
+			nextPoint.setLocation(p.getX(), p.getY() + 1.0);
+			while (board.isEmpty(nextPoint) == true) {
+				Point aPoint = new Point();
+				aPoint.setLocation(nextPoint);
+				locations.add(aPoint);
+				nextPoint.setLocation(nextPoint.getX(), nextPoint.getY() + 1.0);
+			}
+			nextPoint.setLocation(p.getX() + 1.0, p.getY() + 1.0);
+			while (board.isEmpty(nextPoint) == true) {
+				Point aPoint = new Point();
+				aPoint.setLocation(nextPoint);
+				locations.add(aPoint);
+				nextPoint.setLocation(nextPoint.getX() + 1.0, nextPoint.getY() + 1.0);
+			}
+			nextPoint.setLocation(p.getX() + 1, p.getY());
+			while (board.isEmpty(nextPoint) == true) {
+				Point aPoint = new Point();
+				aPoint.setLocation(nextPoint);
+				locations.add(aPoint);
+				nextPoint.setLocation(nextPoint.getX() + 1, nextPoint.getY());
+			}
+			nextPoint.setLocation(p.getX() + 1, p.getY() - 1.0);
+			while (board.isEmpty(nextPoint) == true) {
+				Point aPoint = new Point();
+				aPoint.setLocation(nextPoint);
+				locations.add(aPoint);
+				nextPoint.setLocation(nextPoint.getX() + 1, nextPoint.getY() - 1.0);
+			}
+			nextPoint.setLocation(p.getX(), p.getY() - 1.0);
+			while (board.isEmpty(nextPoint) == true) {
+				Point aPoint = new Point();
+				aPoint.setLocation(nextPoint);
+				locations.add(aPoint);
+				nextPoint.setLocation(nextPoint.getX(), nextPoint.getY() - 1.0);
+			}
+			nextPoint.setLocation(p.getX() - 1, p.getY() - 1.0);
+			while (board.isEmpty(nextPoint) == true) {
+				Point aPoint = new Point();
+				aPoint.setLocation(nextPoint);
+				locations.add(aPoint);
+				nextPoint.setLocation(nextPoint.getX() - 1, nextPoint.getY() - 1.0);
+			}
+			nextPoint.setLocation(p.getX() - 1, p.getY());
+			while (board.isEmpty(nextPoint) == true) {
+				Point aPoint = new Point();
+				aPoint.setLocation(nextPoint);
+				locations.add(aPoint);
+				nextPoint.setLocation(nextPoint.getX() - 1, nextPoint.getY());
+			}
+			nextPoint.setLocation(p.getX() - 1, p.getY() + 1.0);
+			while (board.isEmpty(nextPoint) == true) {
+				Point aPoint = new Point();
+				aPoint.setLocation(nextPoint);
+				locations.add(aPoint);
+				nextPoint.setLocation(nextPoint.getX() - 1, nextPoint.getY() + 1.0);
+			}
+			if (locations.size() != 0) {
+				filled = true;
+			}
+		}
+		return locations;
+	}
+	
+	public static Vector<Point> getEmptySquares(Board board){
+		Vector<Point> points = new Vector<Point>();
+		for(int i=0; i<10;i++){
+			for(int j= 0; j<10;j++){
+				if(board.isEmpty(new Point(i,j))){
+					points.add(new Point(i,j));
+				}
+			}
+		}
+		return points;
+	}
 	
 	/*public ArrayList<Point> allAvailablePoints(Board board){
 		ArrayList<Point> points = new ArrayList<Point>();
@@ -47,20 +146,8 @@ public class Evaluation {
             }
 		}
 	}*/
-	
-	private void getQueens(boolean color) {
-		Vector<Queen> tempqueens = board.getQueens();
-		for (int i = 0; i < tempqueens.size(); i++) {
-			int cntr = 0;
-			if (tempqueens.get(i).isColor() == color) {
-				Queen queenToPlace = tempqueens.get(i);
-				queens.add(cntr, queenToPlace);
-				cntr++;
-			}
-		}
-	}
-	
-	public void getQueenDistances(Board board,boolean colour){
+
+	/*public void getQueenDistances(Board board,boolean colour){
 		int[][] eval = new int[9][9];
 		int[] possibleShortestDistances = new int[4];
 		ArrayList<Point> points = new ArrayList<Point>();
@@ -90,9 +177,9 @@ public class Evaluation {
 				eval[p.x][p.y] = min;
 			}
 		}
-	}
+	}*/
 	
-	public void getKingDistances(Board board,boolean colour){
+	/*public void getKingDistances(Board board,boolean colour){
 		int[][] eval2 = new int[9][9];
 		int[] possibleShortestDistances = new int[4];
 		ArrayList<Point> points = new ArrayList<Point>();
@@ -122,7 +209,7 @@ public class Evaluation {
 				eval2[p.x][p.y] = min;
 			}
 		}
-	}
+	}*/
 	
 	public int BFS(Board board, Point root, Point goal, Vector<Point> locations){
 		Queue<Point> queue = new LinkedList<Point>();
